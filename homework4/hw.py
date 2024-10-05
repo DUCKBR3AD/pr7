6d24873616ca8b3a719a82e26a22b2b79d65643d
DUCKBR3AD <76643973+DUCKBR3AD@users.noreply.github.com>
2024-10-05 17:07:01 +0500

    Update hw.py

--- homework4/hw.py
+++ homework4/hw.py
@@ -1,6 +1,9 @@
 a = int(input("ведите число a: "))
 b = int(input("ведите число b: "))
 c = int(input("ведите число c: "))
-
+try:
+except ValueError:
+  print("Неправильный ввод, введите число")
+  break
 x = a*b +4*c
 print(x)
